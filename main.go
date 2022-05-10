fdf4db66507d05497cd46cce704b9e881f331dfb
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-10 09:24:14 +0430

    cmd init

--- main.go
+++ main.go
@@ -1,31 +1,7 @@
 package main
 
-import (
-	"log"
-
-	"github.com/nats-io/nats.go"
-)
-
-const (
-	nats1 = "nats://0.0.0.0:6222"
-	nats2 = "nats://0.0.0.0:6223"
-)
+import "github.com/amirhnajafiz/j-mirror/internal/cmd"
 
 func main() {
-	{
-		// Connect to NATS server 1
-		nc, _ := nats.Connect(nats1)
-		_, err := nc.JetStream()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
-	{
-		// Connect to NATS server 1
-		nc, _ := nats.Connect(nats2)
-		_, err := nc.JetStream()
-		if err != nil {
-			log.Fatal(err)
-		}
-	}
+	cmd.Execute()
 }
