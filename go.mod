6e54b53167284dc6c958ecd1e2d2f36ae50fe8ab
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-11 15:50:53 +0430

    load configs implemented

--- go.mod
+++ go.mod
@@ -3,6 +3,10 @@ module github.com/amirhnajafiz/j-mirror
 go 1.17
 
 require (
+	github.com/knadh/koanf v1.4.1 // indirect
+	github.com/mitchellh/copystructure v1.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/mitchellh/reflectwalk v1.0.2 // indirect
 	github.com/nats-io/nats.go v1.15.0 // indirect
 	github.com/nats-io/nkeys v0.3.0 // indirect
 	github.com/nats-io/nuid v1.0.1 // indirect
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,5 +1,15 @@
 package config
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/knadh/koanf"
+	"github.com/knadh/koanf/parsers/yaml"
+	"github.com/knadh/koanf/providers/file"
+	"github.com/knadh/koanf/providers/structs"
+)
+
 type Config struct {
 	Nat1        string `koanf:"nat1"`
 	Nat2        string `koanf:"nat2"`
@@ -9,5 +19,24 @@ type Config struct {
 }
 
 func Load() Config {
-	return Default()
+	var instance Config
+
+	k := koanf.New(".")
+
+	// load default
+	if err := k.Load(structs.Provider(Default(), "koanf"), nil); err != nil {
+		_ = fmt.Errorf("error loading deafult: %v\n", err)
+	}
+
+	// load configs file
+	if err := k.Load(file.Provider("config.yaml"), yaml.Parser()); err != nil {
+		_ = fmt.Errorf("error loading config.yaml file: %v\n", err)
+	}
+
+	// unmarshalling
+	if err := k.Unmarshal("", &instance); err != nil {
+		log.Fatalf("error unmarshalling config: %v\n", err)
+	}
+
+	return instance
 }
