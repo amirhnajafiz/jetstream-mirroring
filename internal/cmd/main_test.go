d96dd1e611cbdba1781fa65499f4c23a5cc2b285
amirhnajafiz <najafizadeh21@gmail.com>
2022-05-16 10:51:53 +0430

    subscribing on topic added

--- internal/cmd/main_test.go
+++ internal/cmd/main_test.go
@@ -16,25 +16,48 @@ func Test(t *testing.T) {
 	// loading configs
 	cfg := config.Load()
 
-	// Connect to NATS server
-	nc, err := nats.Connect(cfg.Nats.Nat1)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// creating a jet-stream connection
-	js, err := nc.JetStream()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	// message sending
-	for i := 1; i <= cfg.Tests; i++ {
-		_, err = js.Publish(cfg.Stream.SubjectName, []byte(message))
+	{
+		// Connect to NATS server 1 for publishing
+		nc, err := nats.Connect(cfg.Nats.Nat2)
 		if err != nil {
-			t.Logf("[Test %d/%d] Error: %s\n", i, cfg.Tests, err.Error())
-		} else {
-			t.Logf("[Test %d/%d] OK\n", i, cfg.Tests)
+			t.Fatal(err)
+		}
+
+		// creating a jet-stream connection
+		js, err := nc.JetStream()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// subscribing on subject
+		_, _ = js.Subscribe(cfg.Stream.Subject, func(msg *nats.Msg) {
+			t.Logf("[NATS2][RECIEVE] %s", msg.Data)
+
+			// make acknowledgement
+			_ = msg.Ack()
+		})
+	}
+	{
+		// Connect to NATS server 1 for publishing
+		nc, err := nats.Connect(cfg.Nats.Nat1)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// creating a jet-stream connection
+		js, err := nc.JetStream()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// message sending
+		for i := 1; i <= cfg.Tests; i++ {
+			_, err = js.Publish(cfg.Stream.SubjectName, []byte(message))
+			if err != nil {
+				t.Logf("[NATS1][Test %d/%d] Error: %s\n", i, cfg.Tests, err.Error())
+			} else {
+				t.Logf("[NATS1][Test %d/%d] OK\n", i, cfg.Tests)
+			}
 		}
 	}
 }
